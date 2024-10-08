f3c39301f0241fc1f39840310f7870e1d733654b
Avi7877489 <avi7877489@gmail.com>
2024-10-08 09:26:53 +0530

    Add doc.md & Sample.js chack odd number

---
+++ doc.md
@@ -0,0 +1,7 @@
+# This is a documentation readme file
+
+- Their is Some instruction
+- Download github
+- Create new repository 
+- Add some File
+- Push the code on repo
---
+++ sample.js
@@ -0,0 +1,7 @@
+let arr = [4,5,21,65,3,55,1,65,65]
+
+for(let i = 0;i <= arr.length;i++){
+    if(arr[i] % 2 !== 0){
+        console.log(arr[i], "This is Odd Number")
+    }
+}
